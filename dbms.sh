95e77864423b956123302962f3e92ffefa2e441a
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2024-12-18 15:31:11 +0200

    Refactor delete_from_table function to search properly for the exact matches and delete them

--- dbms.sh
+++ dbms.sh
@@ -38,20 +38,19 @@ delete_from_table() {
     echo -e "Column definitions: $column_definitions" #TODO: remove
 
     column_names=""
-    column_count=1
-    primary_key_index=-1
     IFS=',' read -ra columns <<< "$column_definitions"
     for col in "${columns[@]}"; do
         column_name=$(echo $col | awk '{print $1}')
-        if [ "$column_name" == "$primary_key" ]; then
-            echo -e "Primary key found , column name: $column_name, primary key: $primary_key, index: $column_count" #TODO: remove
-            primary_key_index=$column_count
-        fi
         column_names+="$column_name "
-        ((column_count++))
     done
 
     form_fields=""
+    if [ -z "$primary_key" ]; then
+        yad --error --text="Corrupted table definition: Primary key not found  '$table_to_insert'" --center --width=400 --height=100 --button="OK"
+        return
+    fi
+
+    column_names="$primary_key $column_names"
     for col_name in $column_names; do
         form_fields+="--field=$col_name: "
     done
@@ -83,14 +82,20 @@ delete_from_table() {
     results=""
     while IFS= read -r line; do
         line_data=$(echo $line | tr ' ' '|')
-        for field in "${filled_data_fields[@]}"; do
-            if [[ "$line_data" == *"$field"* ]]; then
-                results+="$line_data\n"
+        match=true
+        for idx in "${filled_fields_indexes[@]}"; do
+            field_value=$(echo $line_data | cut -d'|' -f$idx)
+            if [[ ! " ${filled_data_fields[@]} " =~ " ${field_value} " ]]; then
+                match=false
+                break
             fi
         done
+        if $match; then
+            results+="$line_data\n"
+        fi
     done < <(tail -n +2 "$DB_DIR/$current_db/$table_to_delete_from")
     results=$(echo -e "$results") #to remove the trailing newline
-    results=$(echo "$results" | sed 's/,/ /g')
+    results=$(echo "$results" | sed 's/|/ /g')
     echo -e "Results: $results" #TODO: remove
 
     #if results found then show them to the user to confirm before deletion if the user confirms then delete each record from the table
@@ -101,8 +106,8 @@ delete_from_table() {
         if [ $? -eq 0 ]; then
             while IFS= read -r line; do
                 line_data=$(echo $line | tr ' ' '|')
-                for field in "${filled_data_fields[@]}"; do
-                    if [[ "$line_data" == *"$field"* ]]; then
+                for result in $results; do
+                    if [[ "$line_data" == *"$result"* ]]; then
                         sed -i "/$line/d" "$DB_DIR/$current_db/$table_to_delete_from"
                     fi
                 done
